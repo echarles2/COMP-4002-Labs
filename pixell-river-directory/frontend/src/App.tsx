2140707dd0de18cb77652f8fa91337872aa6aded
ethan <echarles2@academic.rrc.ca>
2026-03-30 18:58:40 -0500

    fixed stuff

--- pixell-river-directory/frontend/src/App.tsx
+++ pixell-river-directory/frontend/src/App.tsx
@@ -1,46 +1,15 @@
-import { useState } from "react";
 import "./styles.css";
 import logo from "./assets/logo.svg";
 
-import Header from "./components/Header.tsx";
-import Directory from "./components/Directory.tsx";
-import Footer from "./components/Footer.tsx";
-import AddEmployeeForm from "./components/AddEmployeeForm.tsx";
-
-import departmentsData from "./data/departments.json";
-import type { Department } from "./types.ts";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import EmployeesPage from "./components/EmployeesPage";
 
 export default function App() {
-  var [departments, setDepartments] = useState<Department[]>(
-    departmentsData as Department[]
-  );
-
-  function addEmployee(firstName: string, lastName: string, departmentName: string) {
-    setDepartments((old) =>
-      old.map((dept) => {
-        if (dept.name !== departmentName) {
-          return dept;
-        }
-
-        return {
-          name: dept.name,
-          employees: dept.employees.concat({
-            firstName: firstName,
-            lastName: lastName || undefined
-          })
-        };
-      })
-    );
-  }
-
   return (
     <div className="page">
       <Header logoSrc={logo} />
-      <Directory departments={departments} />
-      <AddEmployeeForm
-        departments={departments}
-        onAddEmployee={addEmployee}
-      />
+      <EmployeesPage />
       <Footer />
     </div>
   );
