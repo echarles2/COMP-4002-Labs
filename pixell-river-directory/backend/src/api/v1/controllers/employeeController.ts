a37fad3417a7a180efc0fd43a350a7180fa0f586
ethan <echarles2@academic.rrc.ca>
2026-03-30 19:06:16 -0500

    fixed backend controllers to be async

--- pixell-river-directory/backend/src/api/v1/controllers/employeeController.ts
+++ pixell-river-directory/backend/src/api/v1/controllers/employeeController.ts
@@ -5,24 +5,21 @@ import { employeeService } from "../services/employeeService";
 var repo = employeeRepository();
 var service = employeeService(repo);
 
-export function getDepartments(_req: Request, res: Response) {
-  var departments = service.getDepartments();
+export async function getDepartments(_req: Request, res: Response) {
+  var departments = await service.getDepartments();
   res.json(departments);
 }
 
-export function createEmployee(req: Request, res: Response) {
-  var { firstName, lastName, departmentName } = req.body;
-
-  var result = service.createEmployee({
-    firstName: firstName ?? "",
-    lastName: lastName ?? "",
-    departmentName: departmentName ?? ""
+export async function createEmployee(req: Request, res: Response) {
+  var result = await service.createEmployee({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    departmentName: req.body.departmentName
   });
 
   if (!result.ok) {
-    res.status(400).json(result);
-    return;
+    return res.status(400).json(result);
   }
 
-  res.status(201).json(result);
+  return res.json(result);
 }
